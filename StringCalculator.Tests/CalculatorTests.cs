a22ffb7e63f95ac34e4f0a10dd92a5083183b0f9
F30987D <berberiskerdi@gmail.com>
2021-07-21 12:50:26 +0200

    Task 6 done

---
+++ StringCalculator.Tests/CalculatorTests.cs
@@ -0,0 +1,101 @@
+using System;
+using FluentAssertions;
+using Xunit;
+
+namespace StringCalculator.Tests
+{
+    public class CalculatorTests
+    {
+        [Theory]
+        [InlineData("", 0)]
+        [InlineData("1", 1)]
+        [InlineData("1,2", 3)]
+        public void Add_AddsUpToTwoNumber_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+            
+            // Act
+            var result = sut.Add(calculation);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+        
+        [Theory]
+        [InlineData("1,2,3", 6)]
+        [InlineData("10,90,10,20", 130)]
+        public void Add_AddsUpToAnyNumber_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+            
+            // Act
+            var result = sut.Add(calculation);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+        
+        [Theory]
+        [InlineData("1\n2,3", 6)]
+        [InlineData("10\n90,10\n20", 130)]
+        public void Add_AddsNumbersUsingNewLineDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+            
+            // Act
+            var result = sut.Add(calculation);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+        
+        [Theory]
+        [InlineData("//;\n1;2", 3)]
+        [InlineData("//;\n1;2;4", 7)]
+        public void Add_AddsNumbersUsingCustomDelimiter_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+            
+            // Act
+            var result = sut.Add(calculation);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+        
+        [Theory]
+        [InlineData("1,2,-1", "-1")]
+        [InlineData("//;\n1;-2;-4", "-2,-4")]
+        public void Add_ShouldThrowAnException_WhenNegativeNumbersAreUsed(string calculation, string negativeNumbers)
+        {
+            // Arrange
+            var sut = new Calculator();
+            
+            // Act
+            Action action = () => sut.Add(calculation);
+
+            // Assert
+            action.Should().Throw<Exception>()
+                .WithMessage("Negatives not allowed: " + negativeNumbers);
+        }
+
+        [Theory]
+        [InlineData("2,1001", 2)]
+        [InlineData("2,1001,3", 5)]
+        public void Add_AddsNumbersIgnorsNumbersBiggerThan1000_WhenStringIsValid(string calculation, int expected)
+        {
+            // Arrange
+            var sut = new Calculator();
+
+            // Act
+            var result = sut.Add(calculation);
+
+            // Assert
+            result.Should().Be(expected);
+        }
+    }
+}
